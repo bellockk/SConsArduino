20bc239f7840ee4b7e52eac227f4e1084800872c
bellockk <bellockk@segmail.com>
2018-02-16 08:30:06 -0500

    RTC, water level sensors working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+// Arduino
 #include <Arduino.h>
+
+// Display
 #include "LiquidCrystal.h"
 
-// initialize the library with the numbers of the interface pins
+// Water Sensors
+int water_level_1_pin = 0;
+int water_level_2_pin = 1;
+
+// Real Time Clock
+#include <Wire.h>
+#include <RtcDS3231.h>
+
+// Clock
+RtcDS3231<TwoWire> Rtc(Wire);
+
+// Display
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 // Button Pin
@@ -27,6 +41,8 @@ void initVariant() { }
 void setupUSB() __attribute__((weak));
 void setupUSB() { }
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
 int main(void)
 {
     init();
@@ -36,46 +52,91 @@ int main(void)
 #if defined(USBCON)
     USBDevice.attach();
 #endif
-  /////////////////////
-  //      INIT       //
-  /////////////////////
+    /////////////////////
+    //      INIT       //
+    /////////////////////
 
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
 
-  // Print a message to the LCD.
-  lcd.print("Lets Science!");
-  lcd.setCursor(8,1);
-  lcd.print("Alex");
+    // Print a message to the LCD.
+    lcd.print("Lets Go Alex!");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(button_pin, INPUT_PULLUP);  
-    
-  for (;;) {
-      /////////////////////
-      //      LOOP       //
-      /////////////////////
+    // Start the Clock
+    Rtc.Begin();
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
+    RtcDateTime now = Rtc.GetDateTime();
+    if (!Rtc.IsDateTimeValid() || now < compiled) 
+    {
+        Rtc.SetDateTime(compiled);
+    }
+    if (!Rtc.GetIsRunning())
+    {
+        Rtc.SetIsRunning(true);
+    }
+    Rtc.Enable32kHzPin(false);
+    Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone); 
 
-      // Get Button State
-      button_sensed = digitalRead(button_pin);
-      if (button_sensed == HIGH && button_last_state == LOW && millis() - time > debounce) {
-          if (button_state == HIGH){
-              button_state = LOW;
-          }
-          else {
-              button_state = HIGH;
-          }
-          time = millis();
-      }
-      digitalWrite(LED_BUILTIN, button_state);
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(button_pin, INPUT_PULLUP);  
 
-      // 1/Z
-      button_last_state = button_sensed;
+    for (;;) {
+        /////////////////////
+        //      LOOP       //
+        /////////////////////
 
-      // Handle Serial Events (Do Last)
-      if (serialEventRun) serialEventRun();
-  }
+        // Get Button State
+        button_sensed = digitalRead(button_pin);
+        if (button_sensed == HIGH && button_last_state == LOW && millis() - time > debounce) {
+            if (button_state == HIGH){
+                button_state = LOW;
+            }
+            else {
+                button_state = HIGH;
+            }
+            time = millis();
+        }
+        digitalWrite(LED_BUILTIN, button_state);
 
-  return 0;
+        // Water Level Sensor 1
+        int sensed_level_1 = analogRead(water_level_1_pin);
+        int sensed_level_2 = analogRead(water_level_2_pin);
+        lcd.setCursor(9,1);
+        lcd.print(sensed_level_1);
+        lcd.setCursor(13,1);
+        lcd.print(sensed_level_2);
+
+        // Update Clock
+        lcd.setCursor(15,0);
+        if (!Rtc.IsDateTimeValid()) {
+            lcd.print("0");
+        }
+        else {
+            lcd.print("1");
+        }
+        RtcDateTime now = Rtc.GetDateTime();
+        char datestring[20];
+        snprintf_P(datestring, 
+                countof(datestring),
+                PSTR("%02u:%02u:%02u"),
+                now.Hour(),
+                now.Minute(),
+                now.Second() );
+        lcd.setCursor(0,1);
+        lcd.print(datestring);
+
+
+        /////////
+        // 1/Z //
+        /////////
+
+        // Button
+        button_last_state = button_sensed;
+
+        // Handle Serial Events (Do Last)
+        if (serialEventRun) serialEventRun();
+    }
+
+    return 0;
 }
 
