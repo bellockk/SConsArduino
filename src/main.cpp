ad76febd1ee2ef5167b28cd044aa11458c452727
bellockk <bellockk@segmail.com>
2018-02-15 20:16:45 -0500

    Display, and toggle button working

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
 #include <Arduino.h>
+#include "LiquidCrystal.h"
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+
+// Button Pin
+int button_pin = 6;
+
+// Button States
+int button_state = LOW;
+int button_sensed = HIGH;
+int button_last_state = LOW;
+
+// Bounce Parameters
+long time = 0;
+long debounce = 200;
 
 // Declared weak in Arduino.h to allow user redefinitions.
 int atexit(void (* /*func*/ )()) { return 0; }
@@ -13,21 +29,53 @@ void setupUSB() { }
 
 int main(void)
 {
-	init();
+    init();
 
-	initVariant();
+    initVariant();
 
 #if defined(USBCON)
-	USBDevice.attach();
+    USBDevice.attach();
 #endif
-	
-	// setup();
+  /////////////////////
+  //      INIT       //
+  /////////////////////
+
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  // Print a message to the LCD.
+  lcd.print("Lets Science!");
+  lcd.setCursor(8,1);
+  lcd.print("Alex");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(button_pin, INPUT_PULLUP);  
     
-	for (;;) {
-		// loop();
-		if (serialEventRun) serialEventRun();
-	}
-        
-	return 0;
+  for (;;) {
+      /////////////////////
+      //      LOOP       //
+      /////////////////////
+
+      // Get Button State
+      button_sensed = digitalRead(button_pin);
+      if (button_sensed == HIGH && button_last_state == LOW && millis() - time > debounce) {
+          if (button_state == HIGH){
+              button_state = LOW;
+          }
+          else {
+              button_state = HIGH;
+          }
+          time = millis();
+      }
+      digitalWrite(LED_BUILTIN, button_state);
+
+      // 1/Z
+      button_last_state = button_sensed;
+
+      // Handle Serial Events (Do Last)
+      if (serialEventRun) serialEventRun();
+  }
+
+  return 0;
 }
 
